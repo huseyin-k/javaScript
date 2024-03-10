b460226c19cf17f97ac27202eda63d7975b60b0f
Hüseyin kurt <110980957+huseyin-k@users.noreply.github.com>
2024-03-11 01:11:26 +0300

    Add files via upload

---
+++ js/001-hello-world.js
@@ -0,0 +1,11 @@
+// ********** Merhaba Dunya Ornekleri ********** 
+
+// Dokumana(document) Yazdirma Islemi ile Merhaba Dunya:
+// document.write('Merhaba Dunya');
+//document.write('merhaba Dunya');
+//alert(' Merhaba Dunya')
+// Uyari Mesaji (Alert) ile Merhaba Dunya:
+// alert("Merhaba Dunya")
+//console.log('Merhaba Dunyam')
+// console.log ile Merhaba Dunya:
+console.log("Merhaba Dunya")

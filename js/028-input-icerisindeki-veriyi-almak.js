561ed99b0c8bbe6d9845d5599810a0333e173d0b
Hüseyin kurt <110980957+huseyin-k@users.noreply.github.com>
2024-05-30 11:43:55 +0300

    Add files via upload

---
+++ js/028-input-icerisindeki-veriyi-almak.js
@@ -0,0 +1,35 @@
+// INPUT icindeki degeri(value) almak
+
+
+
+
+
+
+let formDOM = document.querySelector("#userForm")
+formDOM.addEventListener('submit', formSubmit)
+
+function formSubmit(event){
+    event.preventDefault()
+    console.log("islem gerceklesti")
+    let scoreInputDOM = document.querySelector('#score')
+    console.log(scoreInputDOM.value)
+    localStorage.setItem('score',scoreInputDOM.value     )
+}
+
+
+
+
+
+/*
+let formDOM = document.querySelector("#userForm")
+formDOM.addEventListener('submit', formSubmit)
+
+
+function formSubmit(event) {
+    event.preventDefault() // default islemi engelledik...
+    console.log("islem gerceklesti")
+    
+    let scoreInputDOM = document.querySelector('#score')
+    console.log(scoreInputDOM.value)
+    localStorage.setItem('score', scoreInputDOM.value)
+}*/

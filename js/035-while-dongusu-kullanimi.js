528a3a415f2ae741b99005f4ea32ecf0a087db2f
Hüseyin kurt <110980957+huseyin-k@users.noreply.github.com>
2024-06-10 10:30:28 +0300

    Add files via upload

---
+++ js/035-while-dongusu-kullanimi.js
@@ -0,0 +1,35 @@
+// While Dongusu
+// https://www.w3schools.com/js/js_loop_while.asp
+
+
+
+/*
+let counter = 0
+
+while(counter < 10 ){
+  console.log(counter)
+  counter++
+}*/
+
+let userName;
+
+while(!userName){
+  userName = prompt("Kullanıcı Adinizi Giriniz: ")
+  console.log(userName)
+}
+
+
+/*
+// let counter = 0;
+
+// while ( counter < 10 ) {
+//   console.log(counter)
+//   counter ++
+// }
+
+let userName = ""
+
+while (!userName) {
+  userName = prompt("Kullanici Adini Giriniz: ")
+  console.log(userName)
+}*/

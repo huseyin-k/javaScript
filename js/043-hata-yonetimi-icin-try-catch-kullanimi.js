9be9c59aa36a0bdaeda8be46f993f8b3bde8bda5
Hüseyin kurt <110980957+huseyin-k@users.noreply.github.com>
2024-06-15 20:50:24 +0300

    Add files via upload

---
+++ js/043-hata-yonetimi-icin-try-catch-kullanimi.js
@@ -0,0 +1,38 @@
+// hata-yonetimi-icin-try-catch-kullanimi
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
+// https://www.w3schools.com/js/js_errors.asp
+
+
+
+let products;
+
+
+try{
+  products.array.forEach(item => console.log(item));
+}catch(error){
+  console.log("error: ", error)
+}
+
+console.log("hata yonetimi duzgun caılisiyor")
+
+let info = "Kodluyoruz"
+console.log(info)
+
+
+
+/*
+let products;
+
+try {
+  products.forEach(item => console.log(item));
+} catch (error) {
+  console.log("error:", error)
+  // products = [1, 2, 3]
+  // products.forEach(item => console.log(item));
+}
+
+
+console.log("hata yonetimi duzgun calisiyor")
+
+let info = "kodluyoruz"
+console.log(info)*/
